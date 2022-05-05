65c5dc9e38736bd3262ae24c928c353118f803b2
Diego Hernández Cortés <795505@unizar.es>
2022-05-05 13:15:02 +0200

    Commit requisito_usuario_3

---
+++ requisitos_usuario/requisito_usuario_3.py
@@ -0,0 +1,83 @@
+# Requisito de usuario Nº3
+# Diego Hernández Cortés
+import os
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class NoExisteConexionServidor1(Exception):
+    """Excepcion que actua cuando no se detecta conexión con el servidor 1"""
+    pass
+
+class NoExisteConexionServidor2(Exception):
+    """Excepcion que actua cuando no se detecta conexión con el servidor 2"""
+    pass
+
+def encontrarIPsServidores():
+    ips = [0,0]
+    archivo = open("/etc/zabbix/zabbix_agentd.conf")
+
+    lineas = archivo.readlines()
+    ipsServidores = lineas[97]
+    ipsServidores = ipsServidores.replace("Server=127.0.0.1, 155.210.71.183, ", "")
+    
+    ips[0] = ipsServidores[0:14]
+    ips[1] = ipsServidores[16:30]
+    return ips
+
+    
+
+
+# Función Ping
+def ping(ip):
+    response = os.system('ping -c 2 ' + ip+ ' > dev.txt')
+
+    if response == 0:
+        return True
+    else:
+        return False
+
+#Comprueba conexión con los servidores
+def comprobarConexion(vector):
+    if ping(vector[0]):
+        print(bcolors.OKBLUE + "EXISTE CONEXION CON EL SERVIDOR 1" + bcolors.ENDC)
+    else:
+        os.system('systemctl stop zabbix-agent > /dev/null')
+        raise NoExisteConexionServidor1
+
+    if ping(vector[1]):
+        print(bcolors.OKBLUE + "EXISTE CONEXION CON EL SERVIDOR 2" + bcolors.ENDC)
+    else:
+        os.system('sudo systemctl stop zabbix-agent > /dev/null')
+        raise NoExisteConexionServidor2
+        
+
+# Función main
+def main() -> int:
+    try:
+       vectorIPs = encontrarIPsServidores()
+       comprobarConexion(vectorIPs)
+
+    except NoExisteConexionServidor1:
+        print(bcolors.FAIL + bcolors.BOLD + "NO EXISTE CONEXION CON EL SERVIDOR 1, FINALIZANDO EJECUCION DEL CLIENTE" + bcolors.ENDC)
+    except NoExisteConexionServidor2:
+        print(bcolors.FAIL + bcolors.BOLD + "NO EXISTE CONEXION CON EL SERVIDOR 2, FINALIZANDO EJECUCION DEL CLIENTE" + bcolors.ENDC)
+
+    return 0
+
+
+# Comienzo de la ejecución
+if __name__ == '__main__':
+
+    main()
